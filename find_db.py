b2ac230204e65759006514fcb303cd7ed900443c
khady <khadimbacke734@gmail.com>
2026-06-26 22:43:15 +0000

    nettoyage : suppression des fichiers de debug temporaires

--- find_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import os
-# Find all db.sqlite3 files
-for root, dirs, files in os.walk('.'):
-    for file in files:
-        if file.endswith('.sqlite3'):
-            print(os.path.join(root, file))
